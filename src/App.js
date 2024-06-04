1378ffb86d51ef2686b0886dd8c46562453bd88d
astonelv <158239756+astonelv@users.noreply.github.com>
2024-06-03 20:32:04 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
 
 // Import components
 import './App.css';
+import Searchbar  from './Search.js';
+import Directory from './Directory.js';
+import Gallery from './Gallery.js';
 
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <Searchbar/>
+        <Directory/>
+        <Gallery/>
+           
       </div>
     </div>
   );
---
+++ src/Search.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+    function Searchbar() {
+        return (
+            <h1>Search</h1>
+        )
+    }
+
+export default Searchbar
+
